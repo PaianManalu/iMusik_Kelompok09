ab6b06b09cf1468612db14e266594d3068ee6b54
paian <128606003+Paiannn@users.noreply.github.com>
2023-05-28 08:03:12 +0700

    selamat datang di imusik_kelompok09
    semangat

--- storage/framework/views/0bae315a8b701c16b55a4d972db348d8.php
+++ storage/framework/views/0bae315a8b701c16b55a4d972db348d8.php
@@ -1,24 +1,32 @@
-<!-- hapus.blade.php -->
 <html>
 
 <head>
     <title>Hapus Video</title>
-    <link rel="stylesheet" href="<?php echo e(asset('css/style.css')); ?>" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
 </head>
 
 <body>
     <div class="container">
         <div class="video-list">
-            <?php $__currentLoopData = $videos; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $video): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            @foreach($videos as $video)
             <div class="video-item">
                 <div class="video-info">
-                    <h3><?php echo e($video->nama); ?></h3>
-                    <p><?php echo e($video->deskripsi); ?></p>
-                    <p>Album: <?php echo e($video->album); ?></p>
-                    <p>Artist: <?php echo e($video->artist); ?></p>
+                    <h3>{{ $video->nama }}</h3>
+                    <p>{{ $video->deskripsi }}</p>
+                    <p>Album: {{ $video->album }}</p>
+                    <p>Artist: {{ $video->artist }}</p>
                 </div>
                 <div class="video-actions">
-                    <form method="POST" action="<?php echo e(route('delete.file', ['file_id' => $video->id])); ?>">
-                        <?php echo csrf_field(); ?>
-                        <?php echo method_field('DELETE'); ?>
-                        <button type="submit" class="btn-delete">Delete<?php /**PATH C:\adminweb\admin\resources\views/hapus.blade.php ENDPATH**/ ?>
+                    <form method="POST" action="{{ route('delete.file', ['file_id' => $video->id]) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn-delete">Delete</button>
+                    </form>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</body>
+
+</html>
